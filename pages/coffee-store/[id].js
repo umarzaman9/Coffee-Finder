0c120a924551ed7cd51c3ada0e3ee23e849d8d76
umarzaman9 <umarofficial9@gmail.com>
2022-07-17 11:16:28 +0500

    adding fallback to dynamic route

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,15 +1,45 @@
 import {useRouter} from 'next/router';
 import Link from 'next/link';
 
-const CoffeeStore = () => {
+import coffeeStoresData from '../../data/coffee-stores.json';
+
+export function getStaticProps(staticProps) {
+	const params = staticProps.params;
+	return {
+		props: {
+			coffeeStore: coffeeStoresData.find(coffeeStore => {
+				return coffeeStore.id.toString() === params.id; //dynamic ID
+			})
+		}
+	}
+}
+
+export function getStaticPaths() {
+	return {
+		paths: [
+		{ params: { id: '0' } },
+		{ params: { id: '1' } }
+		],
+		fallback: true,
+	}
+}
+
+const CoffeeStore = (props) => {
 	const router = useRouter();
+
+	{if(router.isFallback) {
+		return <div>Loading Ho rae hai... </div>
+	}} 
+	console.log("props: dekho: ", props);
 	return (
-				<div>
-	 				Coffeee Store: {router.query.id}
-	 				<Link href="/">
-	 					<a>Back to Home</a>
-	 				</Link>
-	 			</div>
-	 		);
+		<div>
+		Coffeee Store: {router.query.id}
+		<Link href="/">
+		<a>Back to Home</a>
+		</Link>
+		<p> {props.coffeeStore.address} </p>
+		<p> {props.coffeeStore.name} </p>
+		</div>
+		);
 }
 export default CoffeeStore;
